492436015bac2b60a58a2e293a8cbaa9d1f48d19
harsitganz2345 <harunalrasyid272@gmail.com>
2025-06-20 15:43:52 +0700

    mengubah file inde.php

--- inde.php
+++ inde.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello word</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quas repellendus eveniet labore molestiae illo, rerum, harum dignissimos ut delectus doloremque minima impedit amet nobis possimus quo accusantium reiciendis fugiat.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
